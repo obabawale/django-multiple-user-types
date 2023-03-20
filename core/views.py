e232ac1703666379a174d6d2b7989b00a110a948
obabawale <padinality@yahoo.com>
2023-03-20 19:46:38 +0100

    initial commit

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from rest_auth.registration.views import RegisterView
+from core.serializers import (
+    SellerCustomRegistrationSerializer, BuyerCustomRegistrationSerializer
+    )
+
+class SellerRegistrationView(RegisterView):
+    serializer_class = SellerCustomRegistrationSerializer
+
+
+class BuyerRegistrationView(RegisterView):
+    serializer_class = BuyerCustomRegistrationSerializer
